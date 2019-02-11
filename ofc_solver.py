1542df470d4830c5a10209e629b8e5650b41a7f6
mbkuang <35872632+mbkuang@users.noreply.github.com>
2019-02-11 14:44:42 -0600

    Add files via upload

---
+++ ofc_solver.py
@@ -0,0 +1,21 @@
+# *************************************************************************** #
+# 
+# Name
+# 	ofc_solver - provide optimal solutions for open face chinese poker
+# 
+# Synopsis
+# 	python3 ofc_solver.py
+#	python3 ofc_solver.py [NUM PLAYERS]
+#
+# Author
+#	Matthew Kuang
+#
+# *************************************************************************** #
+
+import sys
+
+CONST_VERSION = 1.0
+
+print('\x1b[0;31;40m' + 'Open Face Chinese Poker Solver\n' + '\x1b[0m')
+print('\x1b[0;34;40m' + 'Version ' + str(CONST_VERSION) + '\x1b[0m')
+print('\x1b[0;34;40m' + 'This program was developed by Matthew Kuang' + '\x1b[0m')
